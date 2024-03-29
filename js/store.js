b703ade783db4fbfa8ebc40aab371bde5f0ceacb
francestim <151459060+francestim@users.noreply.github.com>
2024-03-29 18:12:15 +0800

    Update store.js
    put <script src="js/store.js" async></script> on store.html to use remove function on cart

--- js/store.js
+++ js/store.js
@@ -1,18 +1,19 @@
-var removeCartItemButtons = document.getElementsByClassName("fa fa-close")
+var removeCartItemButtons = document.getElementsByClassName('fa fa-close')
 for (var i = 0; i < removeCartItemButtons.length; i++){
     var button = removeCartItemButtons[i]
     button.addEventListener('click', function() {
         var buttonClicked = event.target
         buttonClicked.parentElement.parentElement.remove()
-
+        updateCartTotal()
     })
 }
 
 function updateCartTotal() {
-    var cartItemContainer = document.getElementsByClassName("cart-Popup")[0]
-    cartItemContainer.getElementsByClassName("cart-row")
-    for (var i = 0; i < cart-row.length; i++){
-        var cartRow = cart-row[i]
-        var priceElement = cartRow.getElementsByClassName()
+    var cartItemContainer = document.getElementsByClassName('cart-Popup')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    for (var i = 0; i < cartRows.length; i++){
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('price')[0]
+        
     }
 }   
